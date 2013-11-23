f0ead60ba72df8f2866024d91cacec65a57a1911
TsugumiWinters <TsugumiWinters@github.com>
2013-11-23 20:53:50 +0800

    fix_ui: Fix for Home layout and Book activity

--- src/com/picturebooks/mobilepicturebooks/BookActivity.java
+++ src/com/picturebooks/mobilepicturebooks/BookActivity.java
@@ -3,7 +3,6 @@ package com.picturebooks.mobilepicturebooks;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,19 +19,15 @@ public class BookActivity extends Activity {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userbook);
         
-        Log.d("test", "working 1");
+        imgUser = (ImageView) findViewById(R.id.userbook_img_user);
+        txtName = (TextView) findViewById(R.id.userbook_name);
+        txtAge = (TextView) findViewById(R.id.userbook_age);
+        txtLevel = (TextView) findViewById(R.id.userbook_level);
         
-        //imgUser = (ImageView) findViewById(R.id.userbook_img_user);
-        //txtName = (TextView) findViewById(R.id.userbook_name);
-        //txtAge = (TextView) findViewById(R.id.userbook_age);
-        //txtLevel = (TextView) findViewById(R.id.userbook_level);
-        
-        //Log.d("test", "working 2");
-        
-        //imgUser.setImageResource(getIntent().getIntExtra("user_img", R.drawable.users0));
-        //txtName.setText(getIntent().getStringExtra("user_name"));
-        //txtAge.setText(getResources().getString(R.string.user_age, getIntent().getIntExtra("user_age", 6)));
-        //txtLevel.setText(getIntent().getStringExtra("user_level"));
+        imgUser.setImageResource(getIntent().getIntExtra("user_img", R.drawable.users0));
+        txtName.setText(getIntent().getStringExtra("user_name"));
+        txtAge.setText(getResources().getString(R.string.user_age, getIntent().getIntExtra("user_age", 6)));
+        txtLevel.setText(getIntent().getStringExtra("user_level"));
 	}
 	
 	public void clicked_btnHome(View v) {
