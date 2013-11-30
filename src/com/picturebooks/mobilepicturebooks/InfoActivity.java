142a32b2993882702f9e12f53f049ccf3adee990
TsugumiWinters <TsugumiWinters@github.com>
2013-11-30 20:21:57 +0800

    fix_ui: added info activity

---
+++ src/com/picturebooks/mobilepicturebooks/InfoActivity.java
@@ -0,0 +1,36 @@
+package com.picturebooks.mobilepicturebooks;
+
+import com.picturebooks.mobilepicturebooks.models.ActiveUser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class InfoActivity extends Activity {
+	
+	private boolean openingActivity;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_info);
+		openingActivity = false;
+	}
+	
+	public void clicked_btnBack(View v) {
+		Intent mainIntent = new Intent(this, HomeActivity.class);
+		startActivity(mainIntent);
+		openingActivity = true;
+		finish();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (!openingActivity) {
+			ActiveUser.clearActiveUser(this);
+		}
+	}
+	
+}
