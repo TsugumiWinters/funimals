82d3ad8ce223672992cef7e2bf0fd1c0f02c7a5e
oliver syson <oliversyson@yahoo.com>
2013-12-15 20:48:24 +0800

    removed image in info

--- src/com/swiftshot/funimals/InfoActivity.java
+++ src/com/swiftshot/funimals/InfoActivity.java
@@ -1,38 +1,20 @@
 package com.swiftshot.funimals;
 
-import com.swiftshot.funimals.R;
-import com.swiftshot.funimals.models.ActiveUser;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
+import com.swiftshot.funimals.models.ActiveUser;
 
 public class InfoActivity extends Activity {
 	
 	private boolean openingActivity;
-	private Bitmap icon;
-	private Drawable bg;
-	private BitmapFactory.Options options;
-	private ImageView creditsBackground;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
-		
-		creditsBackground = (ImageView) findViewById(R.id.info_credits);
-		options = new BitmapFactory.Options(); 
-		options.inPurgeable = true;
-		
-		icon = BitmapFactory.decodeResource(getResources(),R.drawable.general_info_credits, options);
-		bg = new BitmapDrawable(getResources(), icon);
-		creditsBackground.setImageDrawable(bg);
 		
 		openingActivity = false;
 	}
